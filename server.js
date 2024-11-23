1109ab339c7d53c55741b9f0d754f66241f970a6
ugly-ceaser <martins.paraclet@yahoo.com>
2024-11-23 12:24:37 +0100

    debuging5 fixed

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 // API Routes
-app.use('/api/responses', routes);
+app.use('/api', routes);
 
 // Serve static files
 app.get('/', (req, res) => {
